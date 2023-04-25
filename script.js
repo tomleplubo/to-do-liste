8c44fb448bd87a29f422bcd3f16430ed7be39a68
tomraviot <tomraviot@gmail.com>
2023-04-25 10:43:03 +0200

    to do list

---
+++ script.js
@@ -0,0 +1,26 @@
+const defaultTodo = [
+    {done: true, name: "Coucou"},
+    {done: true, name: "Coucou"},
+]
+
+const displayTodos = (listTodo) => {
+
+}
+
+
+// #1 selectionner le bouton avec l'id #add, le container #container-list, le input #new
+console.log()
+
+// #2 Afficher les todos dans le container avec une fonction displayTodos
+
+
+// #3 Ajouter un event listener sur le bouton pour ajouter un todo
+
+
+// BONUS
+
+// #5 Faire un filtre 
+
+// #6 Suprimer un éléments
+
+// #4 Cocher les checkbox pour marquer un todo comme fait
